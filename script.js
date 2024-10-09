b692fa1548e36185b2ad361dca7312d61e7e1a71
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2024-10-08 23:03:04 -0300

    basic javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("form");
+
+const Iname = document.querySelector(".name");
+const Iaddress = document.querySelector(".address");
+const Ipassword = document.querySelector(".password");
+const Itel = document.querySelector(".tel");
+
+function register() {
+  fetch("http://localhost:8080/register", {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: "POST",
+    body: JSON.stringify({
+        name: Iname.value,
+        address: Iaddress.value,
+        password: Ipassword.value,
+        phone: Itel.value
+      })
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+}
+
+function clear() {
+    Iname.value = "";
+        Iaddress.value = "";
+        Ipassword.value = "";
+        Itel.value = "";
+};
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  register();
+  clear();
+});
